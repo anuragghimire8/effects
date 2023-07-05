2c0272cc61f60c69b0f97bcbe8283bb06c22d07b
unknown <tankaghimire3@gmail.com>
2023-07-05 16:36:33 +0545

    effects ko

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [num,setNum]=useState(0)
+  const [nums,setNums]=useState(0)
+
+
+  useEffect(()=>{
+    alert('iam cilciked')
+  }, [nums]);
+  return (
+    <div className='App'>
+      <h1>Useeffect {num}</h1>
+      <button onClick={()=>{setNum(num+1)
+      }
+    }
+  >Click Me {num}</button>
+  <br/>
+      <button onClick={()=>{setNums(nums+1)
+      }
+    }
+  >Click Me {nums}</button>
+  
+    </div>
+  )
+}
+
+export default App 
